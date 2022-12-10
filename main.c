6052477cb7e96da125fedba16d823dff132d3c2a
SEOKKAMONI <sj01048260279@gmail.com>
2022-12-10 19:50:34 +0900

    ADD :: MainScreen 추가

--- main.c
+++ main.c
@@ -1,5 +1,40 @@
 #include<stdio.h>
+
+void MainScreen() {
+    int option;
+    printf("==============================\n");
+    printf("------------ Menu ------------\n");
+    printf("\n");
+    printf("[1] To Do List 추가하기\n");
+    printf("\n");
+    printf("[2] To Do List Log 보러가기\n");
+    printf("\n");
+    printf("[3] 나가기\n");
+    printf("\n");
+    printf("==============================\n");
+    scanf("입력하기 : %d", &option);
+    switch (option)
+    {
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    default:
+        printf("그딴거는 없다 게이야..");
+        break;
+    }
+}
+
 int main(void)
 {
-    printf("기본 세팅");
+    MainScreen(); // Main Menu Screen
+    return 0;
 }
