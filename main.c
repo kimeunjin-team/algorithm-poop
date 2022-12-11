a58c8ac566fc584ab35c88a52632a90b37a6f52a
SEOKKAMONI <sj01048260279@gmail.com>
2022-12-11 14:44:15 +0900

    ADD :: 정렬

--- main.c
+++ main.c
@@ -24,6 +24,27 @@ typedef struct
     int length;
 } Todo;
 
+// 정렬
+
+void sort(Queue *q, Todo *t)
+{
+    int i, j, k, temp, key;
+    for(i=0; i<-1; i++)
+    {
+        k=i;
+        for(j=i+1; j<t->length; j++)
+        {
+            if(t->data[j].importance<t->data[k].importance)
+            {
+                k=j;
+            }
+        }
+        temp=q->data[i];
+        q->data[i]=q->data[k];
+        q->data[k]=temp;
+    }
+}
+
 // 큐
 
 void error(char *message)
@@ -50,7 +71,7 @@ void enQueue(Queue *q, element item)
     q->rear++;
     q->data[q->rear] = item; // 삽입
 }
-element deQueue(Queue *q, Todo* t)
+element deQueue(Queue *q, Todo *t)
 {
     if (isEmpty(q))
         error("Queue is empty");
@@ -71,7 +92,9 @@ void MainScreen(Todo *t, Queue *q)
     printf("\n");
     printf("[3] To Do List Check\n");
     printf("\n");
-    printf("[4] 나가기\n");
+    printf("[4] 중요도 순 정렬\n");
+    printf("\n");
+    printf("[5] 나가기\n");
     printf("\n");
     printf("==============================\n");
     printf("입력하기 : ");
@@ -87,7 +110,6 @@ void MainScreen(Todo *t, Queue *q)
         printf("중요도를 입력하세요 : ");
         scanf("%d", &t->data[t->number].importance);
         printf("--------------------------------\n");
-
         enQueue(q, t->number);
 
         t->number++;
@@ -98,7 +120,8 @@ void MainScreen(Todo *t, Queue *q)
         printf("-------- To Do List Log --------\n\n");
         for (int i = 0; i < MAX_Q_SIZE; i++)
         {
-            if(t->data[i].check == 1) {
+            if (t->data[i].check == 1)
+            {
                 printf("[⎷]  %s              중요도: %d\n", t->data[i].todo, *t->data[i].importance);
                 printf("--------------------------------\n");
             }
@@ -118,6 +141,10 @@ void MainScreen(Todo *t, Queue *q)
         deQueue(q, t);
         MainScreen(t, q);
         break;
+    case 4:
+        sort(q, t);
+        MainScreen(t, q);
+        break;
     default:
         break;
     }
@@ -129,10 +156,12 @@ int main(void)
     Queue q;
     t.number = 0;
     t.length = 0;
-    for(int i=0;i<15;i++) {
+
+    for (int i = 0; i < 15; i++)
+    {
         t.data[i].check = 0; // 초기 세팅
     }
- 
+
     initQueue(&q); // queue 초기 세팅
 
     MainScreen(&t, &q); // Main Menu Screen
