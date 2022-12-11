5573f690871d93148b4d03328bfd24202e575c63
SEOKKAMONI <sj01048260279@gmail.com>
2022-12-11 13:02:51 +0900

    CHORE :: To Do List Checking 추가

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX_Q_SIZE 15
+#define MAX_Q_SIZE 10
 
 typedef int element;
 
@@ -14,6 +14,7 @@ typedef struct
 {
     char todo[101];
     int importance[101];
+    int check;
 } list;
 
 typedef struct
@@ -49,12 +50,12 @@ void enQueue(Queue *q, element item)
     q->rear++;
     q->data[q->rear] = item; // 삽입
 }
-element deQueue(Queue *q)
+element deQueue(Queue *q, Todo* t)
 {
     if (isEmpty(q))
         error("Queue is empty");
     q->front++;
-    return q->data[q->front]; // 어느 부분에 삭제했는지 알려주기
+    t->data[q->front].check = 1;
 }
 
 // Main Screen
@@ -68,7 +69,9 @@ void MainScreen(Todo *t, Queue *q)
     printf("\n");
     printf("[2] To Do List Log 보러가기\n");
     printf("\n");
-    printf("[3] 나가기\n");
+    printf("[3] To Do List Check\n");
+    printf("\n");
+    printf("[4] 나가기\n");
     printf("\n");
     printf("==============================\n");
     printf("입력하기 : ");
@@ -83,7 +86,7 @@ void MainScreen(Todo *t, Queue *q)
         scanf("%s", &t->data[t->number].todo);
         printf("중요도를 입력하세요 : ");
         scanf("%d", &t->data[t->number].importance);
-        printf("===============\n");
+        printf("--------------------------------\n");
 
         enQueue(q, t->number);
 
@@ -95,17 +98,24 @@ void MainScreen(Todo *t, Queue *q)
         printf("-------- To Do List Log --------\n\n");
         for (int i = 0; i < MAX_Q_SIZE; i++)
         {
-            if (i <= q->front || i > q->rear)
-            {
-                printf("데 이 터 가 없 습 니 다\n");
+            if(t->data[i].check == 1) {
+                printf("[⎷]  %s              중요도: %d\n", t->data[i].todo, *t->data[i].importance);
                 printf("--------------------------------\n");
+            }
+            else if (i <= q->front || i > q->rear)
+            {
+                continue;
             }
             else
             {
-                printf("%d: %s              중요도: %d\n", i+1, t->data[i].todo, *t->data[i].importance);
+                printf("[ ]  %s              중요도: %d\n", t->data[i].todo, *t->data[i].importance);
                 printf("--------------------------------\n");
             }
         }
+        MainScreen(t, q);
+        break;
+    case 3:
+        deQueue(q, t);
         MainScreen(t, q);
         break;
     default:
@@ -119,6 +129,10 @@ int main(void)
     Queue q;
     t.number = 0;
     t.length = 0;
+    for(int i=0;i<15;i++) {
+        t.data[i].check = 0; // 초기 세팅
+    }
+ 
     initQueue(&q); // queue 초기 세팅
 
     MainScreen(&t, &q); // Main Menu Screen
